49ff685ec0f742c8f07f648085b1c379e9223f6c
Jose Pedro <josepedro88@live.com.pt>
2020-12-28 11:23:04 +0000

    framer motion exemple

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,23 +3,47 @@ import home1 from "../img/home1.png";
 
 import { About, Description, Image, Hide } from "../styles";
 
+import { motion } from "framer-motion";
+
 const AboutSection = () => {
+  const titleAnimation = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.75,
+        ease: "easeOut",
+        staggerChildren: 1,
+        when: "afterChildren",
+      },
+    },
+  };
+
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>come true.</h2>
+            <motion.h2 variants={titleAnimation}>come true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
