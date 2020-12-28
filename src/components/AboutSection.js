31109e40f8a85327b9892ad7caecc35ae695a9c8
Jose Pedro <josepedro88@live.com.pt>
2020-12-28 12:47:43 +0000

    about section animated

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,10 @@
 import React from "react";
 import home1 from "../img/home1.png";
-
+//Styles
 import { About, Description, Image, Hide } from "../styles";
-
+//Animation
 import { motion } from "framer-motion";
+import { titleAnimation, fadeAnimation, photoAnimation } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -11,25 +12,29 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fadeAnimation}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fadeAnimation}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="guy with a camera"
+        />
       </Image>
     </About>
   );
