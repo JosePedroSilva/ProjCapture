b92ba4e2779df1de682689c6dbfddfa2f293250c
Jose Pedro <josepedro88@live.com.pt>
2021-01-01 17:20:23 +0000

    media queries

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -16,6 +16,7 @@ import {
   slider,
   sliderContainer,
 } from "../animation";
+import ScrollTop from "../components/ScrollTop";
 
 const OurWork = () => {
   return (
@@ -58,6 +59,7 @@ const OurWork = () => {
           <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
+      <ScrollTop />
     </Work>
   );
 };
@@ -66,6 +68,9 @@ const Work = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
+  @media (max-width: 1300px) {
+    padding: 2rem 2rem;
+  }
   h2 {
     padding: 1rem 0rem;
   }
